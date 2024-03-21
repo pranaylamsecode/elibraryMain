7c65bedaee3a623f79f57becaf05206523e5596c
pranaylamsecode <pranaylamsecode@gmail.com>
2024-03-21 18:38:41 +0530

    added

--- resources/pos/src/components/newMemberApp/Desclaimer.js
+++ resources/pos/src/components/newMemberApp/Desclaimer.js
@@ -80,7 +80,7 @@ function Terms() {
                         <p>
                             To report abuse regarding copyrighted books on the
                             E-Library portal, don't hesitate to contact us at
-                            info@educron.com,info@createonlineacademy.com. We
+                            info@educron.com, info@createonlineacademy.com. We
                             will thoroughly investigate all reports of abuse and
                             take appropriate actions in accordance with the law.
                         </p>
